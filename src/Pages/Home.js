9e63f1aaed9eccc8d98927bddfc0fd73de194fa9
Ruan Viana Feitosa <vianafeitosaruan@gmail.com>
2024-11-04 16:19:49 -0300

    Criação do Projeto Sonora

---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button, Text, View, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import Card from '../Components/Card';
+
+export default function Home() {
+  return (
+    <View>Hello World</View>
+  )
+}
