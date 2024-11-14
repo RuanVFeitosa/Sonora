62d878d3ff451ddf6ce953d68d47dba89906f0d8
Ruan Viana Feitosa <vianafeitosaruan@gmail.com>
2024-11-14 14:19:57 -0300

    Finalizando a tela Profile || 80%

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, Image, FlatList, StatusBar, SafeAreaView, SectionList, Pressable } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import profile from '../../assets/Taylor.png';
 import notify from '../../assets/notify.png';
@@ -11,25 +12,26 @@ import c1 from '../../assets/c1.png'
 export default function Profile() {
   return (
     <View style={styles.container}>
-        <Image style={styles.profile} source={profile}/>
-        <View style={styles.information}>
-          <Text style={styles.nameInformation}>Name</Text>
-          <Text style={styles.name}>Taylor Swift</Text>
-        </View>
+      <Text style={styles.title}> My Profile </Text>
+      <Pressable style={styles.edit}>
+        <Icon style={styles.editIcon} name='pencil'/>
+        <Text style={styles.editText}>Edit</Text>
+      </Pressable>
+      <Image style={styles.profile} source={profile} />
+      <View style={styles.information}>
+        <Text style={styles.name}>Taylor Swift</Text>
+        <Text style={styles.email}>taylorswift@gmail.com</Text>
+      </View>
       <View style={styles.cards}>
         <View style={styles.column}>
           <View>
             <Pressable onPress={() => navigation.navigate('Playlist')} style={styles.cd1}>
-             
+              <Icon name="heart" size={20} color={'white'} />
               <Text style={styles.textc1}>Gym Cat</Text>
             </Pressable>
           </View>
           <View style={styles.cd1}>
-         
-            <Text style={styles.textc1}>Coffee</Text>
-          </View>
-          <View style={styles.cd1}>
-          
+
             <Text style={styles.textc1}>Coffee</Text>
           </View>
         </View>
@@ -47,14 +49,38 @@ const styles = StyleSheet.create({
   },
 
   title: {
+    fontSize: 30,
+    left: 40,
+    top: 40,
     color: 'white'
+  },
+
+  edit: {
+    padding: 30,
+    width: 68,
+    height: 30,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 5,
+    top: 3,
+    left: 300
+  },
+
+  editIcon: {
+    fontSize: 20,
+    position: 'absolute'
+  },
+
+  editText: {
+    left: 10,
+    color: 'black'
   },
 
   profile: {
     width: 104,
     height: 116,
-    top: 50,
-    left: 30,
+    top: 140,
+    left: 140,
     borderRadius: 100,
     marginRight: 10,
   },
@@ -88,19 +114,28 @@ const styles = StyleSheet.create({
   },
 
   name: {
-    bottom: 50,
-    left: 150,
+    top: 130,
+    left: 110,
 
     fontSize: 30,
-    color: 'grey'
+    color: 'white'
+  },
+
+  email: {
+    top: 130,
+    left: 130,
+
+    fontSize: 12,
+    color: 'white'
   },
 
   cl: {
     color: 'white',
+    width: 30,
     fontWeight: 'bold',
     fontSize: 20,
     top: 60,
-    marginBottom: 20,
+
   },
 
   information: {
@@ -122,14 +157,16 @@ const styles = StyleSheet.create({
   cd1: {
     display: 'flex',
     alignItems: 'center',
-    width: 70,
+    width: 100,
+    margin: 20,
     flexDirection: 'row',
-    height: 50,
+    height: 70,
+    top: 150,
     borderRadius: 12,
     // left: 10,
 
     backgroundColor: '#000000',
-    borderWidth: 2,
+    borderWidth: 1,
     borderColor: 'white',
     marginTop: 46,
   },
@@ -143,8 +180,9 @@ const styles = StyleSheet.create({
 
   textc1: {
     color: 'white',
-    left: 10,
+    left: 1
   },
+
 
 
 })
